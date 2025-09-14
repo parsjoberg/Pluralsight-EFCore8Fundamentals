574582a766f786560556af960cebcbdebe59a3c3
Pär Sjöberg <paer.sjoberg@googlemail.com>
2025-09-14 17:08:09 +0200

    Färdig med M02

--- M02 Building Your FIrst App/Before/PublisherConsole/Program.cs
+++ M02 Building Your FIrst App/Before/PublisherConsole/Program.cs
@@ -1,12 +1,67 @@
-﻿using PublisherData;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using PublisherData;
+using PublisherDomain;
 
 using (PubContext context = new())
 {
-       context.Database.EnsureCreated();
+    context.Database.EnsureCreated();
 }
 
-GetAuthors();
+//GetAuthors();
+//AddAuthor();
+//GetAuthors();
+AddAutorWithBooks();
+GetAuthorsWithBooks();
 
+void AddAutorWithBooks()
+{
+    var author = new Author
+    {
+        FirstName = "John",
+        LastName = "Smith",
+        Books = new List<Book>
+        {
+            new Book { Title = "C# Programming", PublishDate = new DateOnly(2020, 1, 1) },
+            new Book { Title = "ASP.NET Core", PublishDate = new DateOnly(2021, 6, 15) }
+        }
+    };
+    using PubContext context = new();
+    context.Authors.Add(author);
+    context.SaveChanges();
+
+}
+
+void GetAuthorsWithBooks()
+{
+    using PubContext context = new();
+    
+    var authors = context.Authors
+        .Include(a => a.Books)
+        .ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine($"{author.FirstName} {author.LastName}");
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine($"\t{book.Title} - {book.PublishDate}");
+        }
+    }
+}
+
+void AddAuthor()
+{
+    var author = new Author
+    {
+        FirstName = "Jane",
+        LastName = "Doe"
+    };
+    using (PubContext context = new())
+    {
+        context.Authors.Add(author);
+        context.SaveChanges();
+    }
+}
 void GetAuthors()
 {
     using (PubContext context = new())
